e532c129210eab3d1a931cb54318caa6a53da17c
Norbert Melzer <timmelzer@gmail.com>
2016-02-20 23:36:43 +0100

    Added docs to ExParse.RegexParse.parse/1

--- lib/ex_parse/regex_parse.ex
+++ lib/ex_parse/regex_parse.ex
@@ -1,4 +1,6 @@
 defmodule ExParse.RegexParse do
+
+  @doc "Parses a given string or char list into its internal representation."
   def parse(string) when is_binary(string), do: string |> to_char_list |> parse
   def parse(string) when is_list(string), do: string |> :regex_parse.parse_string
 end
