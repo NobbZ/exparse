5c39b1a4c57ea1d15df35b2631ee75ba6b05642c
Norbert Melzer <timmelzer@gmail.com>
2016-02-21 11:17:09 +0100

    ExParse.RegexParse.parse/1: improved docs

--- lib/ex_parse/regex_parse.ex
+++ lib/ex_parse/regex_parse.ex
@@ -1,7 +1,11 @@
 defmodule ExParse.RegexParse do
   @moduledoc "Parser for regular expressions."
   
-  @doc "Parses a given string or char list into its internal representation."
+  @doc """
+  Parses a given string or char list into its internal representation.
+
+  Takes the `string` to parse and returns the internal representation.
+  """
   def parse(string) when is_binary(string), do: string |> to_char_list |> parse
   def parse(string) when is_list(string), do: string |> :regex_parse.parse_string
 end
