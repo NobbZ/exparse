7da9673cbb077ad76ea6a1d2e15657e9ef97a084
Norbert Melzer <timmelzer@gmail.com>
2016-10-02 23:57:46 +0200

    updating dependencies

--- mix.exs
+++ mix.exs
@@ -27,14 +27,14 @@ defmodule ExParse.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:earmark,  "~> 0.1",   only: :dev},
-     {:ex_doc,   "~> 0.11",  only: :dev},
+    [{:earmark,  "~> 1.0",   only: :dev},
+     {:ex_doc,   "~> 0.14",  only: :dev},
      {:dialyxir, "~> 0.3",   only: :dev},
 
      # {:excheck,  "~> 0.3",   only: :test},
      # {:triq, github: "krestenkrab/triq", only: :test},
 
-     {:inch_ex,  "~> 0.5.1", only: :dev},
-     {:credo,    "~> 0.4.1", only: :dev}]
+     {:inch_ex,  "~> 0.5", only: :dev},
+     {:credo,    "~> 0.4", only: :dev}]
   end
 end
