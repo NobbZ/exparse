de3eb8df2e172e3ab446ce97e242a8a32757e32f
Norbert Melzer <timmelzer@gmail.com>
2016-10-03 11:46:51 +0200

    Set elixir version to 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExParse.Mixfile do
   def project do
     [app: :exparse,
      version: "0.0.1",
-     elixir: (if Mix.env == :test, do: "~> 1.3", else: "~> 1.2"),
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
